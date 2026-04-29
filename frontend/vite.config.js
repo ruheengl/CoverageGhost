6fdbefbba0b084026a399737a155c4e347bd787d
Sam-0505 <samirpatro2001@gmail.com>
2026-04-28 20:26:19 -0500

    added new backend key

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://coverageghost.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
